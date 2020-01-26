5e71d372f68776c3c3cdf4e83b778eb4dc240134
Vibeesh Kamalakannan <41962112+vibeeshK@users.noreply.github.com>
2020-01-26 21:17:29 +0530

    SourceCleanup

--- ESPoT/src/main/java/espot/OSHandler.java
+++
@@ -1,8 +0,0 @@
-package espot;
-
-public interface OSHandler {
-	/*
-	 * Interface mandates for OS handlers
-	 */
-	public void openFileToView(String inFileString);
-}
--- OpenDeClouder/CommonOpenCldFns/src/main/java/commonTechs/TransferObject.java
+++
@@ -1,9 +0,0 @@
-package commonTechs;
-
-public class TransferObject {
-	/*
-	 * A convenience class
-	 */
-	public Object sourceObj;
-	public Object destinationObj;
-}
