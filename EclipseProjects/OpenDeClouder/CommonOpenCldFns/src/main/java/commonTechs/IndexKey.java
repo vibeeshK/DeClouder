bef121bd1c65e407a0c0fd89f8e589138bc80bf0
Vibeesh Kamalakannan <41962112+vibeeshK@users.noreply.github.com>
2020-01-28 02:42:50 +0530

    source refresh

---
+++ EclipseProjects/OpenDeClouder/CommonOpenCldFns/src/main/java/commonTechs/IndexKey.java
@@ -0,0 +1,10 @@
+package commonTechs;
+// This class is not used
+public class IndexKey {
+	public String indexString;
+	public int indexPointer;
+	public IndexKey(String inIndexString,int inIndexPointer) {
+		indexString = inIndexString;
+		indexPointer = inIndexPointer;
+	}
+}
