562b01bfa8e23c94c731d510ff249f7520372816
Vibeesh Kamalakannan <41962112+vibeeshK@users.noreply.github.com>
2020-04-12 00:57:46 +0530

    ExecRefresh11Apr2020b

--- SourceCode/Archive/ESPoT/java/espot/PrimerDocInterface.java
+++
@@ -1,9 +0,0 @@
-package espot;
-
-public interface PrimerDocInterface {
-	/*
-	 * Interface mandates for the primer doc consumer.
-	 * Primer doc is the meta data holder of the artifact packaged in a zip file
-	 */	
-	void absorbIncomingItemPojo (ItemPojo inItemPojo);
-}
